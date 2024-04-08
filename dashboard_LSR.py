5f40c34650e1a68b491c3cc61a4a8bfb26b6984b
Rodrigo Mendoza <rod_c0@hotmail.com>
2024-04-08 13:26:22 -0600

    generated different structure for dash app

--- dashboard_LSR.py
+++ dashboard_LSR.py
@@ -14,14 +14,17 @@ import matplotlib.pyplot as plt
 import plotly.express  as px
 import os
 from dash import Dash, html, dcc, Input, Output
+from pathlib import Path
+from utils.utilities import next_free_port
 
+parent_path =str(Path(__file__).parent.absolute())
 
-pn.extension('tabulator', sizing_mode="stretch_width")
+# list_of_sets = []
+list_of_sets = os.listdir(parent_path + '/Data') 
 
-list_of_sets = os.listdir('C:/Users/rod_c/Documents/Python/LSR/Data') 
-expansion = 'DMU'
+expansion = 'BRO'
 
-stx_df = pd.read_json('C:/Users/rod_c/Documents/Python/Datos/' + expansion + '.json') #Se lee el archivo JSON de 
+stx_df = pd.read_json(parent_path + '/Data/' + expansion + '.json') #Se lee el archivo JSON de 
 a_jason=json.dumps(stx_df['data']['cards'])
 a_jason2=json.loads(a_jason)
 df=pd.DataFrame.from_dict(a_jason2, orient="columns")
@@ -36,7 +39,7 @@ df['toughness'].replace(to_replace='1+*',value='1',inplace = True)
 df['power']=df['power'].astype(float)
 df['toughness']=df['toughness'].astype(float)
 
-mtg_limited_app = Dash()
+mtg_limited_app = Dash(__name__)
 
 #Layout
 dd_list = []
@@ -44,13 +47,21 @@ dd_list.append('all')
 for i in df['rarity'].unique():
     dd_list.append(i)
     
-    
-rarity_checklist = dcc.Checklist(id='r_checklist',value='all',options=dd_list,
+print(dd_list)
+rarity_checklist = dcc.Checklist(id='r_checklist',options=dd_list,
                                  labelStyle={'display': 'block'},style={"overflow":"auto"})
 
-mtg_limited_app.layout = html.Div([
+mtg_limited_app.layout = html.Div(children=[
     
-    html.Div(children=[html.H1('Limited Set Review')],style={'text-align': 'center'}),
+    html.Div(children=[
+        html.H1('Limited Set Review'),
+        html.Div(children=[
+            html.H3('Select Expansion', style={'height':'1vh','margin':'5px'}),
+            dcc.Dropdown(id='sets_dd', options=list_of_sets, style={'display':'inline-block', 'height':'5vh',
+                                                                'margin-top':'1vh', 'width':'27vw', 'text-align':'center'})
+        ],style={'position':'absolute','top':'0','left':'0', 'width':'27vw', 'margin':'0', 'padding-left':'10px'})
+        
+    ],style={'text-align': 'center'}),
     
     html.Div(children=[html.Div(children=[
             dcc.Graph(id='Power_Stats',style={'height':'40vh'}),
@@ -61,13 +72,25 @@ mtg_limited_app.layout = html.Div([
             html.H3(children='Rarity',style={}),
             rarity_checklist
             ],style={'padding': 10, 'flex': 1})
-    ],style={'display': 'flex', 'flex-direction': 'row'})
+    ],style={'display': 'flex', 'flex-direction': 'row'}),
+    dcc.Store(id='json_storage')
     
 ], style={'display': 'flex', 'flex-direction': 'column'})
 
 @mtg_limited_app.callback(
+    outputs=dict(d=Output('json_storage','data')),
+    inputs=dict(sel=Input('sets_dd','value')),
+    prevent_initial_call=True
+)
+
+def data_load(sel):
+    
+    return
+
+@mtg_limited_app.callback(
     Output(component_id='Power_Stats',component_property='figure'),
-    Input(component_id='r_checklist', component_property='value')
+    Input(component_id='r_checklist', component_property='value'),
+    prevent_initial_call=True
 )
 
 def update_graph(selected_rarity):
@@ -85,7 +108,8 @@ def update_graph(selected_rarity):
 
 @mtg_limited_app.callback(
     Output(component_id='Toughness_Stats',component_property='figure'),
-    Input(component_id='r_checklist', component_property='value')
+    Input(component_id='r_checklist', component_property='value'),
+    prevent_initial_call=True
 )
 
 def update_graph2(selected_rarity):
@@ -105,5 +129,5 @@ def update_graph2(selected_rarity):
 mtg_limited_app.scripts.config.serve_locally = True
 mtg_limited_app.css.config.serve_locally = True
 
-if __name__ == '__main__':
-    mtg_limited_app.run_server(debug=False, use_reloader=False, port=8150)
+user_port = next_free_port()
+mtg_limited_app.run(debug=True, port= user_port)
