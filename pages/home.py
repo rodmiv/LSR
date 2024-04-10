de50753af8a685d940cef7867cb3c1cf2b8d45a4
Rodrigo Mendoza <rod_c0@gmail.com>
2024-04-09 22:01:18 -0600

    started changes in home page

--- pages/home.py
+++ pages/home.py
@@ -1,9 +1,31 @@
 import dash
 import dash_bootstrap_components as dbc
-from dash import html
+from dash import html, dcc
 
 dash.register_page(__name__,path='/')
 
 layout = html.Div(children=[
-    html.H1('Welcome')
-])
+    
+    dbc.Row(children=[
+        dbc.Col(
+            dbc.Card(
+                dbc.CardBody(
+                    [
+                        html.H1('Welcome!'),
+                        html.P(
+                            '''
+                                Welcome to the limited set reviewer tool. The following is my attempt at creating an automation of some basic analytics and statistics of MTG sets for limited play purposes.
+                            ''',
+                        ),
+                    ]
+                ),
+                className='w-75 m-auto',
+            ),
+            class_name='col-6'
+        ),
+        dbc.Col(
+            html.Img(src='./images/card.png', alt='cards')
+            ,class_name='col-6'
+        ),
+    ],className='w-100 p-5')
+],className='text-center')
