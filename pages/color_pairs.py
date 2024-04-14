fdcff53ccba3aae98b67a931266f0b7068ae4ba6
Rodrigo Mendoza <rod_c0@gmail.com>
2024-04-14 11:48:23 -0600

    added placeholder images to color pairs

--- pages/color_pairs.py
+++ pages/color_pairs.py
@@ -10,5 +10,24 @@ dash.register_page(
 
 layout = html.Div(children=[
     html.H1('Color Pairs',className='text-center'),
-    
-])
+    dbc.Row(
+        children=[
+            dbc.Col(className='col-4'),
+            dbc.Col(
+                children=[
+                    html.Img(src='./assets/images/unh-44-richard-garfield-ph-d.png', className='w-75 ')
+                ]
+                ,className='col-4'
+            ),
+            dbc.Col(
+                children=[
+                    html.Img(src='./assets/images/unh-134-city-of-ass.png', className='w-75 ')
+                ]
+                ,className='col-4'
+            ),
+        ],
+        className='p-5 d-flex justify-content-center w-100 m-0'
+    ),
+
+],
+    className='mb-5')
